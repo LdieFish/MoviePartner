0e91211ca34b483c7b97706c8f6f8c0a14818d5e
dingjiacheng <1793698080@qq.com>
2026-05-25 15:58:37 +0800

    初始化项目

---
+++ src/main/java/com/movie/moviecompanion/MovieCompanionApplication.java
@@ -0,0 +1,13 @@
+package com.movie.moviecompanion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCompanionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieCompanionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/movie/moviecompanion/MovieCompanionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.moviecompanion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCompanionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
