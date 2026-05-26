5d8b84450c833ea715cfa2632bfb96e05d1adcbc
fish <2779528110@qq.com>
2026-05-26 18:21:17 +0800

    feat(database): 集成 MySQL 数据库并添加代码生成器
    - 在 application.yml 中配置 MySQL 数据源连接信息
    - 添加 SQL 初始化脚本创建数据库和用户表
    - 集成 MyBatis-Flex 并配置 Mapper 扫描
    - 添加 MyBatis 代码生成器支持
    - 更新 pom.xml 依赖版本并添加数据库相关依赖
    - 新增用户注册请求 DTO 类

---
+++ src/main/java/com/movie/moviecompanion/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.movie.moviecompanion.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.movie.moviecompanion.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author master
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
