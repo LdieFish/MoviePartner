06bb416efd968ce738a1b42a6a13811b71fcc3b1
fish <2779528110@qq.com>
2026-06-18 15:30:36 +0800

    feat(core): 微服务 - 截图服务

---
+++ movie-partner-microservice/screenshot/src/main/java/com/movie/moviecompanionai/MovieCompanionScreenshotlication.java
@@ -0,0 +1,13 @@
+package com.movie.moviecompanionai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCompanionScreenshotlication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieCompanionScreenshotlication.class, args);
+    }
+
+}
--- movie-partner-microservice/user/src/main/java/com/movie/moviecompanionuser/MovieCompanionUserlication.java
+++ movie-partner-microservice/user/src/main/java/com/movie/moviecompanionuser/MovieCompanionUserlication.java
@@ -8,10 +8,10 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @MapperScan("com.movie.moviecompanionuser.mapper")
 @ComponentScan("com.movie")
-public class MovieCompanionApplication {
+public class MovieCompanionUserlication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MovieCompanionApplication.class, args);
+        SpringApplication.run(MovieCompanionUserlication.class, args);
     }
 
 }
